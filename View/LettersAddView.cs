10dff7b47e4f030d674b48e539b1897084455194
Dabid <Dabid@NONAME>
2023-09-05 19:45:13 +0800

    remove "ding" sound when registering a letter tag

--- View/LettersAddView.cs
+++ View/LettersAddView.cs
@@ -124,81 +124,88 @@ namespace AgRecords.View
 
         private void txtBoxTags_KeyPress(object sender, KeyPressEventArgs e)
         {
+            //setting the e.Handled to true removes the 'ding' sound
+            e.Handled = false;
             if (e.KeyChar == (char)Keys.Enter)
             {
-                string tagText = txtBoxTags.Text.Trim();
-                int controlHeight = 20;
-
-                if (!string.IsNullOrEmpty(tagText))
-                {
-                    // Create a panel to hold the label and remove button
-                    Panel panel = new Panel();
-                    panel.BackColor = Color.LightGray;
-                    panel.Margin = new Padding(3);
-                    panel.Height = controlHeight; // Adjust the height as needed
-
-                    // Create a label for the tag text
-                    Label label = new Label();
-                    label.Text = tagText;
-                    label.AutoSize = true;
-                    label.Dock = DockStyle.Left;
-                    label.TextAlign = ContentAlignment.MiddleCenter;
-
-                    // Create a button for removing the tag
-                    Button removeButton = new Button();
-                    removeButton.Text = "X";
-                    removeButton.BackColor = Color.Red;
-                    removeButton.ForeColor = Color.White;
-                    removeButton.FlatStyle = FlatStyle.Flat;
-                    removeButton.Dock = DockStyle.Right;
-                    removeButton.Font = new Font(removeButton.Font.FontFamily, 6, FontStyle.Regular); // Adjust font size
-                    removeButton.Width = 20; // Adjust the width as needed
-                    removeButton.Height = controlHeight + 2;
-
-                    // Define the click event for the remove button
-                    removeButton.Click += (s, args) =>
-                    {
-                        // Find the TagItem associated with the panel and remove it
-                        TagItem itemToRemove = tagItems.FirstOrDefault(item => item.Panel == panel);
-                        if (itemToRemove != null)
-                        {
-                            tagItems.Remove(itemToRemove);
-                            flowLayoutPanelTags.Controls.Remove(panel);
-                        }
-                    };
-
-                    // Add label and remove button to the panel
-                    panel.Controls.Add(label);
-                    panel.Controls.Add(removeButton);
-                    panel.Width = label.Width + controlHeight;
-
-
-                    // Calculate the X position for the panel
-                    int xPosition = 0;
-                    foreach (var tagItem in tagItems)
-                    {
-                        xPosition += tagItem.Panel.Width + tagItem.Panel.Margin.Left + tagItem.Panel.Margin.Right;
-                    }
-
-                    // Set the location of the panel
-                    panel.Location = new Point(xPosition, 0);
-
-                    // Create a TagItem instance and add it to the list
-                    TagItem newTagItem = new TagItem
-                    {
-                        Text = tagText,
-                        Panel = panel
-                    };
-                    tagItems.Add(newTagItem);
-
-                    // Add the panel to the FlowLayoutPanel
-                    flowLayoutPanelTags.Controls.Add(panel);
-
-                    // Clear the text box
-                    txtBoxTags.Clear();
-
-                }
+                e.Handled = registerTag();
             }
+        }
+
+        private Boolean registerTag()
+        {
+            string tagText = txtBoxTags.Text.Trim();
+            int controlHeight = 20;
+
+            if (!string.IsNullOrEmpty(tagText))
+            {
+                // Create a panel to hold the label and remove button
+                Panel panel = new Panel();
+                panel.BackColor = Color.LightGray;
+                panel.Margin = new Padding(3);
+                panel.Height = controlHeight; // Adjust the height as needed
+
+                // Create a label for the tag text
+                Label label = new Label();
+                label.Text = tagText;
+                label.AutoSize = true;
+                label.Dock = DockStyle.Left;
+                label.TextAlign = ContentAlignment.MiddleCenter;
+
+                // Create a button for removing the tag
+                Button removeButton = new Button();
+                removeButton.Text = "X";
+                removeButton.BackColor = Color.Red;
+                removeButton.ForeColor = Color.White;
+                removeButton.FlatStyle = FlatStyle.Flat;
+                removeButton.Dock = DockStyle.Right;
+                removeButton.Font = new Font(removeButton.Font.FontFamily, 6, FontStyle.Regular); // Adjust font size
+                removeButton.Width = 20; // Adjust the width as needed
+                removeButton.Height = controlHeight + 2;
+
+                // Define the click event for the remove button
+                removeButton.Click += (s, args) =>
+                {
+                    // Find the TagItem associated with the panel and remove it
+                    TagItem itemToRemove = tagItems.FirstOrDefault(item => item.Panel == panel);
+                    if (itemToRemove != null)
+                    {
+                        tagItems.Remove(itemToRemove);
+                        flowLayoutPanelTags.Controls.Remove(panel);
+                    }
+                };
+
+                // Add label and remove button to the panel
+                panel.Controls.Add(label);
+                panel.Controls.Add(removeButton);
+                panel.Width = label.Width + controlHeight;
+
+
+                // Calculate the X position for the panel
+                int xPosition = 0;
+                foreach (var tagItem in tagItems)
+                {
+                    xPosition += tagItem.Panel.Width + tagItem.Panel.Margin.Left + tagItem.Panel.Margin.Right;
+                }
+
+                // Set the location of the panel
+                panel.Location = new Point(xPosition, 0);
+
+                // Create a TagItem instance and add it to the list
+                TagItem newTagItem = new TagItem
+                {
+                    Text = tagText,
+                    Panel = panel
+                };
+                tagItems.Add(newTagItem);
+
+                // Add the panel to the FlowLayoutPanel
+                flowLayoutPanelTags.Controls.Add(panel);
+
+                // Clear the text box
+                txtBoxTags.Clear();
+            }
+            return true;
         }
 
     }
