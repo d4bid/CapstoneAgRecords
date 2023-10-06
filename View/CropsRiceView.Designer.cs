af086d399a6921309331bee55e70871991c7e7ae
Darynne Keith Asis <kirakeitisaic@gmail.com>
2023-10-07 00:22:09 +0700

    Add Reporting

--- View/CropsRiceView.Designer.cs
+++ View/CropsRiceView.Designer.cs
@@ -28,47 +28,15 @@
         /// </summary>
         private void InitializeComponent()
         {
-            DataGridViewCellStyle dataGridViewCellStyle1 = new DataGridViewCellStyle();
-            DataGridViewCellStyle dataGridViewCellStyle2 = new DataGridViewCellStyle();
-            dgvCropsRice = new DataGridView();
+            DataGridViewCellStyle dataGridViewCellStyle3 = new DataGridViewCellStyle();
+            DataGridViewCellStyle dataGridViewCellStyle4 = new DataGridViewCellStyle();
             panel1 = new Panel();
-            ((System.ComponentModel.ISupportInitialize)dgvCropsRice).BeginInit();
-            panel1.SuspendLayout();
+            panel2 = new Panel();
+            panel3 = new Panel();
+            dgvRicePlanting = new DataGridView();
+            panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dgvRicePlanting).BeginInit();
             SuspendLayout();
-            // 
-            // dgvCropsRice
-            // 
-            dgvCropsRice.AllowUserToAddRows = false;
-            dgvCropsRice.AllowUserToDeleteRows = false;
-            dgvCropsRice.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
-            dgvCropsRice.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
-            dgvCropsRice.BackgroundColor = Color.White;
-            dgvCropsRice.BorderStyle = BorderStyle.None;
-            dataGridViewCellStyle1.Alignment = DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle1.BackColor = Color.FromArgb(42, 148, 214);
-            dataGridViewCellStyle1.Font = new Font("Century Gothic", 9.75F, FontStyle.Regular, GraphicsUnit.Point);
-            dataGridViewCellStyle1.ForeColor = Color.White;
-            dataGridViewCellStyle1.SelectionBackColor = Color.FromArgb(52, 74, 95);
-            dataGridViewCellStyle1.SelectionForeColor = Color.White;
-            dataGridViewCellStyle1.WrapMode = DataGridViewTriState.True;
-            dgvCropsRice.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
-            dgvCropsRice.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridViewCellStyle2.Alignment = DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle2.BackColor = Color.FromArgb(240, 241, 242);
-            dataGridViewCellStyle2.Font = new Font("Century Gothic", 9.75F, FontStyle.Regular, GraphicsUnit.Point);
-            dataGridViewCellStyle2.ForeColor = Color.FromArgb(52, 74, 95);
-            dataGridViewCellStyle2.SelectionBackColor = Color.FromArgb(78, 177, 203);
-            dataGridViewCellStyle2.SelectionForeColor = Color.FromArgb(52, 74, 95);
-            dataGridViewCellStyle2.WrapMode = DataGridViewTriState.False;
-            dgvCropsRice.DefaultCellStyle = dataGridViewCellStyle2;
-            dgvCropsRice.Location = new Point(0, 99);
-            dgvCropsRice.Name = "dgvCropsRice";
-            dgvCropsRice.ReadOnly = true;
-            dgvCropsRice.RowHeadersVisible = false;
-            dgvCropsRice.RowHeadersWidth = 51;
-            dgvCropsRice.RowTemplate.Height = 25;
-            dgvCropsRice.Size = new Size(1139, 520);
-            dgvCropsRice.TabIndex = 21;
             // 
             // panel1
             // 
@@ -77,28 +45,84 @@
             panel1.Location = new Point(0, 0);
             panel1.Name = "panel1";
             panel1.Padding = new Padding(0, 0, 0, 10);
-            panel1.Size = new Size(1139, 90);
+            panel1.Size = new Size(1595, 90);
             panel1.TabIndex = 22;
+            // 
+            // panel2
+            // 
+            panel2.BackColor = Color.White;
+            panel2.Controls.Add(dgvRicePlanting);
+            panel2.Dock = DockStyle.Fill;
+            panel2.Location = new Point(0, 90);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(1595, 836);
+            panel2.TabIndex = 23;
+            // 
+            // panel3
+            // 
+            panel3.Dock = DockStyle.Top;
+            panel3.Location = new Point(0, 90);
+            panel3.Name = "panel3";
+            panel3.Size = new Size(1595, 5);
+            panel3.TabIndex = 24;
+            // 
+            // dgvRicePlanting
+            // 
+            dgvRicePlanting.AllowUserToAddRows = false;
+            dgvRicePlanting.AllowUserToDeleteRows = false;
+            dgvRicePlanting.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            dgvRicePlanting.BackgroundColor = Color.White;
+            dgvRicePlanting.ColumnHeadersBorderStyle = DataGridViewHeaderBorderStyle.Single;
+            dataGridViewCellStyle3.Alignment = DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle3.BackColor = Color.FromArgb(43, 121, 223);
+            dataGridViewCellStyle3.Font = new Font("Microsoft Sans Serif", 10.8F, FontStyle.Regular, GraphicsUnit.Point);
+            dataGridViewCellStyle3.ForeColor = Color.White;
+            dataGridViewCellStyle3.Padding = new Padding(0, 10, 0, 10);
+            dataGridViewCellStyle3.SelectionBackColor = Color.FromArgb(255, 221, 100);
+            dataGridViewCellStyle3.SelectionForeColor = Color.FromArgb(0, 35, 76);
+            dataGridViewCellStyle3.WrapMode = DataGridViewTriState.True;
+            dgvRicePlanting.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle3;
+            dgvRicePlanting.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCellStyle4.Alignment = DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle4.BackColor = Color.White;
+            dataGridViewCellStyle4.Font = new Font("Roboto", 10.8F, FontStyle.Regular, GraphicsUnit.Point);
+            dataGridViewCellStyle4.ForeColor = Color.Black;
+            dataGridViewCellStyle4.SelectionBackColor = Color.FromArgb(255, 221, 100);
+            dataGridViewCellStyle4.SelectionForeColor = Color.FromArgb(0, 35, 76);
+            dataGridViewCellStyle4.WrapMode = DataGridViewTriState.False;
+            dgvRicePlanting.DefaultCellStyle = dataGridViewCellStyle4;
+            dgvRicePlanting.GridColor = Color.FromArgb(239, 239, 239);
+            dgvRicePlanting.Location = new Point(12, 15);
+            dgvRicePlanting.Name = "dgvRicePlanting";
+            dgvRicePlanting.ReadOnly = true;
+            dgvRicePlanting.RowHeadersVisible = false;
+            dgvRicePlanting.RowHeadersWidth = 51;
+            dgvRicePlanting.RowTemplate.Height = 40;
+            dgvRicePlanting.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            dgvRicePlanting.Size = new Size(1571, 809);
+            dgvRicePlanting.TabIndex = 23;
             // 
             // CropsRiceView
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.FromArgb(239, 239, 239);
-            ClientSize = new Size(1139, 619);
-            Controls.Add(dgvCropsRice);
+            ClientSize = new Size(1595, 926);
+            Controls.Add(panel3);
+            Controls.Add(panel2);
             Controls.Add(panel1);
             Name = "CropsRiceView";
             Text = "CropsRiceView";
             Load += CropsRiceView_Load;
-            ((System.ComponentModel.ISupportInitialize)dgvCropsRice).EndInit();
-            panel1.ResumeLayout(false);
+            panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)dgvRicePlanting).EndInit();
             ResumeLayout(false);
         }
 
         #endregion
-
-        private DataGridView dgvCropsRice;
         private Panel panel1;
+        private Panel panel2;
+        private Panel panel3;
+        private DataGridView dgvRicePlanting;
     }
 }
