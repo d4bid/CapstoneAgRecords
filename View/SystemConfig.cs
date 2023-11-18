81c9495bacf18a500650fe479fea0f7b1342047b
Darynne Keith Asis <kirakeitisaic@gmail.com>
2023-11-18 10:41:02 +0700

    Revert "Revert "Revert "system config: restore"""
    This reverts commit 96780238245cd25ee6029f79586aa1b6252e574e.

--- View/SystemConfig.cs
+++ View/SystemConfig.cs
@@ -1,5 +1,4 @@
-﻿using AgRecords.Model;
-using AgRecords.Properties;
+﻿using AgRecords.Properties;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -9,7 +8,6 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using static System.Windows.Forms.VisualStyles.VisualStyleElement.StartPanel;
 
 namespace AgRecords.View
 {
@@ -47,31 +45,6 @@ namespace AgRecords.View
         {
             DialogResult = DialogResult.Cancel;
             Close();
-        }
-
-        private void btnRestore_Click(object sender, EventArgs e)
-        {
-            try
-            {
-                // Set up the OpenFileDialog
-                OpenFileDialog restore = new OpenFileDialog();
-                restore.InitialDirectory = "C:\\";
-                restore.Title = "Database Restore";
-                restore.CheckFileExists = true;
-                restore.CheckPathExists = true;
-                restore.DefaultExt = "sql";
-                restore.Filter = "sql files (*.sql)|*.sql|All files (*.*)|*.* ";
-                restore.RestoreDirectory = true;
-
-                if (restore.ShowDialog() == DialogResult.OK)
-                {
-                    MessageBox.Show("SQL file restored succesfully.", "Message", MessageBoxButtons.OK, MessageBoxIcon.Information);
-                }
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message, "Restoration Failed", MessageBoxButtons.OK, MessageBoxIcon.Warning);
-            }
         }
     }
 }
