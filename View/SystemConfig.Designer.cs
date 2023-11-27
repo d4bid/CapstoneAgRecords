36c4e01fae8e2031af5d12aaceb64aa318776fea
Darynne Keith Asis <kirakeitisaic@gmail.com>
2023-11-27 11:39:33 +0700

    Modify SystemConfig UI 1

--- View/SystemConfig.Designer.cs
+++ View/SystemConfig.Designer.cs
@@ -46,6 +46,7 @@
             txtDatabase = new TextBox();
             rectangleBlue4 = new RectangleBlue();
             label4 = new Label();
+            panel1.SuspendLayout();
             SuspendLayout();
             // 
             // btnCancel
@@ -55,7 +56,7 @@
             btnCancel.FlatStyle = FlatStyle.Flat;
             btnCancel.Font = new Font("Segoe UI Semibold", 10.8F, FontStyle.Bold, GraphicsUnit.Point);
             btnCancel.ForeColor = Color.White;
-            btnCancel.Location = new Point(580, 329);
+            btnCancel.Location = new Point(240, 406);
             btnCancel.Name = "btnCancel";
             btnCancel.Size = new Size(166, 34);
             btnCancel.TabIndex = 4;
@@ -70,7 +71,7 @@
             btnSave.FlatStyle = FlatStyle.Flat;
             btnSave.Font = new Font("Segoe UI Semibold", 10.8F, FontStyle.Bold, GraphicsUnit.Point);
             btnSave.ForeColor = Color.White;
-            btnSave.Location = new Point(402, 329);
+            btnSave.Location = new Point(62, 406);
             btnSave.Name = "btnSave";
             btnSave.Size = new Size(166, 34);
             btnSave.TabIndex = 5;
@@ -82,7 +83,7 @@
             // 
             txtServerAddress.BorderStyle = BorderStyle.None;
             txtServerAddress.Font = new Font("Segoe UI", 11.7F, FontStyle.Regular, GraphicsUnit.Point);
-            txtServerAddress.Location = new Point(408, 52);
+            txtServerAddress.Location = new Point(68, 129);
             txtServerAddress.Margin = new Padding(5, 3, 3, 3);
             txtServerAddress.Name = "txtServerAddress";
             txtServerAddress.Size = new Size(333, 26);
@@ -90,7 +91,7 @@
             // 
             // rectangleBlue1
             // 
-            rectangleBlue1.Location = new Point(401, 47);
+            rectangleBlue1.Location = new Point(61, 124);
             rectangleBlue1.Name = "rectangleBlue1";
             rectangleBlue1.Size = new Size(344, 38);
             rectangleBlue1.TabIndex = 46;
@@ -100,7 +101,7 @@
             lblUsername.AutoSize = true;
             lblUsername.Font = new Font("Segoe UI Semibold", 10.2F, FontStyle.Bold, GraphicsUnit.Point);
             lblUsername.ForeColor = Color.FromArgb(43, 121, 223);
-            lblUsername.Location = new Point(400, 19);
+            lblUsername.Location = new Point(60, 96);
             lblUsername.Name = "lblUsername";
             lblUsername.Size = new Size(89, 23);
             lblUsername.TabIndex = 45;
@@ -134,7 +135,7 @@
             btnRestore.IconColor = Color.FromArgb(43, 121, 223);
             btnRestore.IconFont = FontAwesome.Sharp.IconFont.Auto;
             btnRestore.IconSize = 100;
-            btnRestore.Location = new Point(570, 401);
+            btnRestore.Location = new Point(603, 269);
             btnRestore.Name = "btnRestore";
             btnRestore.Size = new Size(100, 100);
             btnRestore.TabIndex = 48;
@@ -146,7 +147,7 @@
             label1.AutoSize = true;
             label1.Font = new Font("Segoe UI Semibold", 10.2F, FontStyle.Bold, GraphicsUnit.Point);
             label1.ForeColor = Color.FromArgb(43, 121, 223);
-            label1.Location = new Point(420, 439);
+            label1.Location = new Point(577, 372);
             label1.Name = "label1";
             label1.Size = new Size(144, 23);
             label1.TabIndex = 49;
@@ -154,17 +155,31 @@
             // 
             // panel1
             // 
+            panel1.Controls.Add(txtDatabase);
+            panel1.Controls.Add(txtServerAddress);
+            panel1.Controls.Add(rectangleBlue4);
+            panel1.Controls.Add(btnCancel);
+            panel1.Controls.Add(label4);
+            panel1.Controls.Add(btnSave);
+            panel1.Controls.Add(txtPassword);
+            panel1.Controls.Add(lblUsername);
+            panel1.Controls.Add(rectangleBlue3);
+            panel1.Controls.Add(rectangleBlue1);
+            panel1.Controls.Add(label3);
+            panel1.Controls.Add(Username);
+            panel1.Controls.Add(txtUsername);
+            panel1.Controls.Add(rectangleBlue2);
             panel1.Dock = DockStyle.Left;
             panel1.Location = new Point(0, 0);
             panel1.Name = "panel1";
-            panel1.Size = new Size(379, 500);
+            panel1.Size = new Size(478, 500);
             panel1.TabIndex = 50;
             // 
             // txtUsername
             // 
             txtUsername.BorderStyle = BorderStyle.None;
             txtUsername.Font = new Font("Segoe UI", 11.7F, FontStyle.Regular, GraphicsUnit.Point);
-            txtUsername.Location = new Point(409, 121);
+            txtUsername.Location = new Point(69, 198);
             txtUsername.Margin = new Padding(5, 3, 3, 3);
             txtUsername.Name = "txtUsername";
             txtUsername.Size = new Size(333, 26);
@@ -172,7 +187,7 @@
             // 
             // rectangleBlue2
             // 
-            rectangleBlue2.Location = new Point(402, 116);
+            rectangleBlue2.Location = new Point(62, 193);
             rectangleBlue2.Name = "rectangleBlue2";
             rectangleBlue2.Size = new Size(344, 38);
             rectangleBlue2.TabIndex = 53;
@@ -182,7 +197,7 @@
             Username.AutoSize = true;
             Username.Font = new Font("Segoe UI Semibold", 10.2F, FontStyle.Bold, GraphicsUnit.Point);
             Username.ForeColor = Color.FromArgb(43, 121, 223);
-            Username.Location = new Point(401, 88);
+            Username.Location = new Point(61, 165);
             Username.Name = "Username";
             Username.Size = new Size(87, 23);
             Username.TabIndex = 52;
@@ -192,7 +207,7 @@
             // 
             txtPassword.BorderStyle = BorderStyle.None;
             txtPassword.Font = new Font("Segoe UI", 11.7F, FontStyle.Regular, GraphicsUnit.Point);
-            txtPassword.Location = new Point(408, 197);
+            txtPassword.Location = new Point(68, 274);
             txtPassword.Margin = new Padding(5, 3, 3, 3);
             txtPassword.Name = "txtPassword";
             txtPassword.Size = new Size(333, 26);
@@ -200,7 +215,7 @@
             // 
             // rectangleBlue3
             // 
-            rectangleBlue3.Location = new Point(401, 192);
+            rectangleBlue3.Location = new Point(61, 269);
             rectangleBlue3.Name = "rectangleBlue3";
             rectangleBlue3.Size = new Size(344, 38);
             rectangleBlue3.TabIndex = 56;
@@ -210,7 +225,7 @@
             label3.AutoSize = true;
             label3.Font = new Font("Segoe UI Semibold", 10.2F, FontStyle.Bold, GraphicsUnit.Point);
             label3.ForeColor = Color.FromArgb(43, 121, 223);
-            label3.Location = new Point(400, 164);
+            label3.Location = new Point(60, 241);
             label3.Name = "label3";
             label3.Size = new Size(82, 23);
             label3.TabIndex = 55;
@@ -220,7 +235,7 @@
             // 
             txtDatabase.BorderStyle = BorderStyle.None;
             txtDatabase.Font = new Font("Segoe UI", 11.7F, FontStyle.Regular, GraphicsUnit.Point);
-            txtDatabase.Location = new Point(409, 275);
+            txtDatabase.Location = new Point(69, 352);
             txtDatabase.Margin = new Padding(5, 3, 3, 3);
             txtDatabase.Name = "txtDatabase";
             txtDatabase.Size = new Size(333, 26);
@@ -228,7 +243,7 @@
             // 
             // rectangleBlue4
             // 
-            rectangleBlue4.Location = new Point(402, 270);
+            rectangleBlue4.Location = new Point(62, 347);
             rectangleBlue4.Name = "rectangleBlue4";
             rectangleBlue4.Size = new Size(344, 38);
             rectangleBlue4.TabIndex = 59;
@@ -238,7 +253,7 @@
             label4.AutoSize = true;
             label4.Font = new Font("Segoe UI Semibold", 10.2F, FontStyle.Bold, GraphicsUnit.Point);
             label4.ForeColor = Color.FromArgb(43, 121, 223);
-            label4.Location = new Point(401, 242);
+            label4.Location = new Point(61, 319);
             label4.Name = "label4";
             label4.Size = new Size(81, 23);
             label4.TabIndex = 58;
@@ -250,29 +265,17 @@
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.White;
             ClientSize = new Size(800, 500);
-            Controls.Add(txtDatabase);
-            Controls.Add(rectangleBlue4);
-            Controls.Add(label4);
-            Controls.Add(txtPassword);
-            Controls.Add(rectangleBlue3);
-            Controls.Add(label3);
-            Controls.Add(txtUsername);
-            Controls.Add(rectangleBlue2);
-            Controls.Add(Username);
             Controls.Add(panel1);
             Controls.Add(label1);
             Controls.Add(btnRestore);
             Controls.Add(btnExit);
-            Controls.Add(txtServerAddress);
-            Controls.Add(rectangleBlue1);
-            Controls.Add(lblUsername);
-            Controls.Add(btnSave);
-            Controls.Add(btnCancel);
             FormBorderStyle = FormBorderStyle.None;
             Name = "SystemConfig";
             StartPosition = FormStartPosition.CenterParent;
             Text = "SystemConfig";
             Load += SystemConfig_Load;
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
             ResumeLayout(false);
             PerformLayout();
         }
